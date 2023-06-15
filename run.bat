6cda1f4f55202777e320be51cd4d4472c1d687c6
Hafidh Musyafa <j5musyaf@gmail.com>
2023-06-15 15:06:03 +0700

    First Commit

---
+++ run.bat
@@ -0,0 +1,2 @@
+start cmd.exe /c "cd .\web\dep_vue && npm run serve"
+start cmd.exe /c "ping localhost -n 25 -w 60000 > nul && start http://localhost:8080/"
---
+++ web/dep_vue/src/components/CourseComment.vue
@@ -0,0 +1,19 @@
+<template>
+    <article class="media box">
+      <div class="media-content">
+        <div class="content">
+          <p>
+            <strong>{{ comment.name }}</strong> {{ comment.createdAt }}<br>
+            {{ comment.content }}
+          </p>
+        </div>
+      </div>
+    </article>
+  </template>
+  
+  <script>
+  export default {
+    props: ['comment']
+  }
+  </script>
+  
