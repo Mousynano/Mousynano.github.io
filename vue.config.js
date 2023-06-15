d83e092f993f1220865d76e5f85eb6a4e961d3e4
Hafidh Musyafa <91863507+J58C@users.noreply.github.com>
2023-06-15 14:25:32 +0700

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,4 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+module.exports = {
+  publicPath: process.env.NODE_ENV === "production" ? "WeKTek" : "/",
+};
 
-export default defineConfig({
-    base 'WeKTek'
-    plugins: [vue()]
-})
